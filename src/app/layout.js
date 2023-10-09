c4e5705f0c38dd095e419e508ad1cb6eb6036f8b
AnggiSatriaa <anggi.satria@talentinsider@gmail.com>
2023-10-09 22:22:43 +0700

    [Anggi Satria]: section init pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>
 
 const inter = Inter({ subsets: ['latin'] })
 
